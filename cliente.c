48b380b31fba5511892b1fbf2b2d6e89003aa5a3
Marthyna Luiza Weber <marthyna.weber@aprix.com.br>
2025-04-25 12:28:17 -0300

    Add error checks

--- cliente.c
+++ cliente.c
@@ -1,13 +1,16 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "discovery.h"
 
@@ -17,24 +20,20 @@
 #define PACKET_TYPE_REQ_ACK 4
 #define MAX_HISTORY 100000000
 
-struct requisicao
-{
+struct requisicao {
     uint32_t value;
 };
 
-struct requisicao_ack
-{
+struct requisicao_ack {
     uint32_t seqn;
     uint32_t num_reqs;
     uint64_t total_sum;
 };
 
-typedef struct __packet
-{
+typedef struct __packet {
     uint16_t type;
     uint32_t seqn;
-    union
-    {
+    union {
         struct requisicao req;
         struct requisicao_ack ack;
     } data;
@@ -42,36 +41,32 @@ typedef struct __packet
 
 uint32_t valores_enviados[MAX_HISTORY] = {0};
 int sock;
-struct sockaddr_in serveraddr;
-socklen_t addrlen = sizeof(serveraddr);
+struct sockaddr_in servaddr, from;
+struct hostent *server;
+socklen_t addrlen = sizeof(servaddr);
 uint32_t seqn = 1;
 
 pthread_mutex_t ack_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t ack_cond = PTHREAD_COND_INITIALIZER;
 int ack_recebido = 0;
 
-void current_time(char *buf, size_t len)
-{
+void current_time(char *buf, size_t len) {
     time_t now = time(NULL);
     strftime(buf, len, "%Y-%m-%d %H:%M:%S", localtime(&now));
 }
 
-void *interface_thread(void *arg)
-{
-    while (1)
-    {
+void *interface_thread(void *arg) {
+    while (1) {
         packet ack;
         ssize_t len = recvfrom(sock, &ack, sizeof(ack), 0, NULL, NULL);
-        if (len > 0 && ack.type == PACKET_TYPE_REQ_ACK)
-        {
+        if (len > 0 && ack.type == PACKET_TYPE_REQ_ACK) {
             pthread_mutex_lock(&ack_lock);
-            if (ack.seqn == seqn)
-            {
+            if (ack.seqn == seqn) {
                 char timebuf[64];
                 current_time(timebuf, sizeof(timebuf));
                 uint32_t value = valores_enviados[ack.seqn];
                 printf("%s server %s id_req %u value %u num_reqs %u total_sum %lu\n",
-                       timebuf, inet_ntoa(serveraddr.sin_addr), ack.seqn, value, ack.data.ack.num_reqs,
+                       timebuf, inet_ntoa(servaddr.sin_addr), ack.seqn, value, ack.data.ack.num_reqs,
                        ack.data.ack.total_sum);
                 ack_recebido = 1;
                 pthread_cond_signal(&ack_cond);
@@ -82,35 +77,52 @@ void *interface_thread(void *arg)
     return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         fprintf(stderr, "Uso: %s <porta>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    int port = atoi(argv[1]);
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
 
+    // Pega a porta do argumento
+    int port = atoi(argv[1]);
+
+    // Cria o socket com domínio AF_INET e tipo SOCK_DGRAM (protocolo UDP)
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("Cannot create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Cria o container genérico que permite que o SO identifique a família do endereço
+    memset((char *)&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero(&(servaddr.sin_zero), 8);
+
+    // Vincular o socket à porta
+    if (bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("Cannot bind socket");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+    // Configura o socket para enviar pacotes de broadcast
     int broadcast = 1;
     setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
 
     // Descoberta do servidor
-    if (discovery_send_broadcast(sock, port, &serveraddr) < 0)
-    {
+    if (discovery_send_broadcast(sock, port, &servaddr) < 0) {
         fprintf(stderr, "Falha na descoberta do servidor\n");
         exit(EXIT_FAILURE);
     }
 
     char timebuf[64];
     current_time(timebuf, sizeof(timebuf));
-    printf("%s server_addr %s\n", timebuf, inet_ntoa(serveraddr.sin_addr));
+    printf("%s server_addr %s\n", timebuf, inet_ntoa(servaddr.sin_addr));
 
     pthread_t tid;
     pthread_create(&tid, NULL, interface_thread, NULL);
 
-    while (1)
-    {
+    while (1) {
         uint32_t value;
         if (scanf("%u", &value) != 1)
             break;
@@ -125,25 +137,22 @@ int main(int argc, char *argv[])
 
         pthread_mutex_lock(&ack_lock);
         ack_recebido = 0;
-        sendto(sock, &req, sizeof(req), 0, (struct sockaddr *)&serveraddr, addrlen);
+        sendto(sock, &req, sizeof(req), 0, (struct sockaddr *)&servaddr, addrlen);
 
-        while (!ack_recebido)
-        {
+        while (!ack_recebido) {
             struct timespec timeout;
             clock_gettime(CLOCK_REALTIME, &timeout);
-            timeout.tv_nsec += 10000000; // 10ms = 10.000.000ns
-            if (timeout.tv_nsec >= 1000000000)
-            {
+            timeout.tv_nsec += 10000000;  // 10ms = 10.000.000ns
+            if (timeout.tv_nsec >= 1000000000) {
                 timeout.tv_nsec -= 1000000000;
                 timeout.tv_sec++;
             }
 
             pthread_cond_timedwait(&ack_cond, &ack_lock, &timeout);
 
-            if (!ack_recebido)
-            {
+            if (!ack_recebido) {
                 // Reenviar a requisição
-                sendto(sock, &req, sizeof(req), 0, (struct sockaddr *)&serveraddr, addrlen);
+                sendto(sock, &req, sizeof(req), 0, (struct sockaddr *)&servaddr, addrlen);
             }
         }
         pthread_mutex_unlock(&ack_lock);
@@ -151,7 +160,7 @@ int main(int argc, char *argv[])
     }
 
     close(sock);
-    pthread_cancel(tid); // Encerrar a thread interface de maneira segura
+    pthread_cancel(tid);  // Encerrar a thread interface de maneira segura
     pthread_join(tid, NULL);
     return 0;
 }
--- discovery.c
+++ discovery.c
@@ -1,36 +1,33 @@
 #include "discovery.h"
+
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 // Envia uma mensagem de descoberta em broadcast (cliente)
-int discovery_send_broadcast(int sock, uint16_t port, struct sockaddr_in *serveraddr)
-{
+int discovery_send_broadcast(int sock, uint16_t port, struct sockaddr_in *serveraddr) {
     struct sockaddr_in bcastaddr = {
         .sin_family = AF_INET,
         .sin_port = htons(port),
-        .sin_addr.s_addr = inet_addr("255.255.255.255")};
+        .sin_addr.s_addr = inet_addr("192.168.2.255")};
 
     discovery_packet pkt = {.type = PACKET_TYPE_DISCOVERY};
 
     // Envia o pacote de descoberta
-    if (sendto(sock, &pkt, sizeof(pkt), 0, (struct sockaddr *)&bcastaddr, sizeof(bcastaddr)) < 0)
-    {
+    if (sendto(sock, &pkt, sizeof(pkt), 0, (struct sockaddr *)&bcastaddr, sizeof(bcastaddr)) < 0) {
         perror("Erro ao enviar pacote de descoberta");
         return -1;
     }
 
     // Aguarda a resposta do servidor
     socklen_t addrlen = sizeof(*serveraddr);
-    if (recvfrom(sock, &pkt, sizeof(pkt), 0, (struct sockaddr *)serveraddr, &addrlen) < 0)
-    {
+    if (recvfrom(sock, &pkt, sizeof(pkt), 0, (struct sockaddr *)serveraddr, &addrlen) < 0) {
         perror("Erro ao receber resposta do servidor");
         return -1;
     }
 
-    if (pkt.type == PACKET_TYPE_DISCOVERY_ACK)
-    {
+    if (pkt.type == PACKET_TYPE_DISCOVERY_ACK) {
         return 0;
     }
 
@@ -39,13 +36,11 @@ int discovery_send_broadcast(int sock, uint16_t port, struct sockaddr_in *server
 }
 
 // Responde a uma mensagem de descoberta (servidor)
-void discovery_handle_request(int sock, struct sockaddr_in *cliaddr, socklen_t len)
-{
+void discovery_handle_request(int sock, struct sockaddr_in *cliaddr, socklen_t len) {
     discovery_packet resp = {.type = PACKET_TYPE_DISCOVERY_ACK};
 
     // Envia a resposta ao cliente
-    if (sendto(sock, &resp, sizeof(resp), 0, (struct sockaddr *)cliaddr, len) < 0)
-    {
+    if (sendto(sock, &resp, sizeof(resp), 0, (struct sockaddr *)cliaddr, len) < 0) {
         perror("Erro ao enviar resposta de descoberta");
     }
 }
