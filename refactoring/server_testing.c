0d53d19df9780071bc10c7125eb1091d530a64fe
marcus-c-farias <marcusczupriniakifarias@gmail.com>
2025-07-05 14:59:50 -0300

    working

--- refactoring/server_testing.c
+++ refactoring/server_testing.c
@@ -16,24 +16,24 @@
 #define PACKET_TYPE_DESC_ACK 3
 #define PACKET_TYPE_REQ_ACK 4
 
-struct requisicao {
-    uint32_t value; // Valor da requsição
-};
+// struct requisicao {
+//     uint32_t value; // Valor da requsição
+// };
 
-struct requisicao_ack {
-    uint32_t seqn; //Número de sequência que está sendo feito o ack
-    uint32_t num_reqs; // Quantidade de requisições
-    uint64_t total_sum; // Valor da soma agregada até o momento
-};
+// struct requisicao_ack {
+//     uint32_t seqn; //Número de sequência que está sendo feito o ack
+//     uint32_t num_reqs; // Quantidade de requisições
+//     uint64_t total_sum; // Valor da soma agregada até o momento
+// };
 
-typedef struct __packet {
-    uint16_t type; // Tipo do pacote (DESC | REQ | DESC_ACK | REQ_ACK )
-    uint32_t seqn; //Número de sequência de uma requisição
-    union {
-        struct requisicao req;
-        struct requisicao_ack ack;
-    } data;
-} packet;
+// typedef struct __packet {
+//     uint16_t type; // Tipo do pacote (DESC | REQ | DESC_ACK | REQ_ACK )
+//     uint32_t seqn; //Número de sequência de uma requisição
+//     union {
+//         struct requisicao req;
+//         struct requisicao_ack ack;
+//     } data;
+// } packet;
 
 typedef struct {
     struct sockaddr_in addr; //end ip do cliente
